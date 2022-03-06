580409b697df3bdfb2c508aea50ff1bca47a1b60
rwieners <rene.wieners@gmail.com>
2022-03-06 20:59:22 +0100

    inheritance

--- src/vererbung1/Dog.java
+++ src/vererbung1/Dog.java
@@ -2,6 +2,9 @@ package vererbung1;
 
 public class Dog extends Animal{
 
+    public Dog(String color){
+        this.color = color;
+    }
 
     public void bark(){
         System.out.print("\nWau Wau");
--- src/vererbung1/Program.java
+++ src/vererbung1/Program.java
@@ -3,12 +3,15 @@ package vererbung1;
 public class Program {
 
     public static void main(String[] args) {
-        Dog dog = new Dog();
+        Dog dog = new Dog("Schwarz");
         dog.bark();
+        System.out.println(dog.color);
 
         Cat cat = new Cat();
         cat.purr();
 
         //Test for git
+
+
     }
 }
